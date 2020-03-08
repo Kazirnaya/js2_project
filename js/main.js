1688f6647ad7eee824bd42bc227eb41ed60cef72
Kazirnaya <anastasiya.ka@mail.ru>
2020-03-09 01:54:45 +0300

    hw2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,82 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
+class ProductsList{
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchProducts();
+    } 
+    
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
 
-const renderProduct = (item) => {
-    return `<div class="product-item">
-                <img src="img/picture.jpg" alt="#" class="product-img">
-                <h3 class="product-item-H3">${item.title}</h3>
-                <p class="product-item-p">${item.price}</p>
-                <button class="buy-btn">Добавить</button>
+    render() {
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend',productObj.render())
+        }
+    }
+    
+    sumOfProducts() {
+        this.sum = 0;
+        this.goods.forEach(good => {
+            this.sum += good.price;
+        });
+        console.log(this.sum);
+    }
+    
+}
+
+
+class ProductItem{
+	constructor(product, img = 'https://placehold.it/200x150'){
+		this.title = product.title;
+		this.price = product.price;
+		this.id = product.id;
+		this.img = img;
+		
+	}
+	
+	render(){
+		 return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
-    productsList.forEach(element => {
-        document.querySelector('.products').innerHTML += element;
-    });
-};
+	}
+}
 
-renderPage(products);
+let list = new ProductsList();
+list.render();
+list.sumOfProducts()
+
+class Bin{
+    /* методы:
+    (добавление элемента в корзину будет в классе ProductsList)
+    удаление товара из корзины
+    увеличение/уменьшение количества товаров одного типа
+   
+    */
+}
+
+class BinItem{
+    /* методы:
+    рендер элемента корзины
+*/
+}
+
+
+
+
+    
+
+
+
